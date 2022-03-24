2852f7ed39a4c1b3a3e4531cbc2d70a00bd20906
Aishu181 <89151694+Aishu181@users.noreply.github.com>
2022-03-24 17:20:05 +0530

    Add files via upload

---
+++ jokes-app/src/main/java/com/example/demo/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	 private final JokeService jokeService;
+
+	    public JokeController(JokeService jokeService) {
+	        this.jokeService = jokeService;
+	    }
+
+	    @RequestMapping({"/", ""})
+	    public String showJoke(Model model) {
+
+	        model.addAttribute("joke", jokeService.getJoke());
+
+	        return "index";
+	    }
+	
+}
